eaf9810ecc104ec9f70a88f790e5f11aee609bd0
Rebeca <rebeca.vcarvalho78@gmail.com>
2023-08-02 16:46:41 -0300

    It was about a Zebra

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    return (
+    <nav>
+        <li>Hi</li>
+        <li>Hello</li>
+        <li>You're welcome</li>
+    </nav>
+    );
+}
+
+export default Header;
